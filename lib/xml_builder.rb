c7d0b87fba7dc4559e8eaa7b77113db1ac552942
Randy Eppinger <Randy.Eppinger@leandog.com>
2015-02-05 21:32:35 -0500

    Initial commit

---
+++ lib/xml_builder.rb
@@ -0,0 +1,67 @@
+puts 'Starting with examples taken from http://www.rubydoc.info/github/sparklemotion/nokogiri/Nokogiri/XML/Builder'
+
+puts '===Basic example==='
+builder = Nokogiri::XML::Builder.new do
+  root {
+    products {
+      widget {
+        id_ "10"
+        name "Awesome widget"
+      }
+    }
+  }
+end
+puts builder.to_xml
+
+puts '===Add encoding to xml tag==='
+builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do
+  root {
+    products {
+      widget {
+        id_ "10"
+        name "Awesome widget"
+      }
+    }
+  }
+end
+puts builder.to_xml
+
+puts '===Adding an attribute to a node==='
+builder = Nokogiri::XML::Builder.new do
+  root {
+    products {
+      widget(category:'stuff') {
+        id_ "10"
+        name "Awesome widget"
+      }
+    }
+  }
+end
+puts builder.to_xml
+
+puts '===Adding an undefined namespace to a node==='
+#This will generate an error because the namespace has not been defined
+builder = Nokogiri::XML::Builder.new do
+  root('xmlns:foo' => 'bar') {
+    products { |products|
+      products['foo'].widget(category:'stuff') {
+        id_ "10"
+        name "Awesome widget"
+      }
+    }
+  }
+end
+puts 'ArgumentError: Namespace foo has not been defined'
+
+puts '===Adding a properly defined namespace to a node==='
+builder = Nokogiri::XML::Builder.new do
+  root('xmlns:foo' => 'bar') {
+    products { |products|
+      products['foo'].widget(category:'stuff') {
+        id_ "10"
+        name "Awesome widget"
+      }
+    }
+  }
+end
+puts builder.to_xml
